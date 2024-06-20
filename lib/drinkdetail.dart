acedd82d3563884a6aeb8cf052cec85c0095ec68
rifqidias <rifqi.p@allianz.co.id>
2024-06-20 21:07:20 +0700

    add detail

--- lib/drinkdetail.dart
+++ lib/drinkdetail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsample/service/detail_drink_api.dart';
+import 'package:newsample/utils/const.dart';
 
 import 'main.dart';
 
@@ -16,129 +17,159 @@ class _DrinkDetailState extends State<DrinkDetail> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: fetchCocktail(widget.id),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Errornya: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('No data found'));
-          } else {
-            dynamic cocktail = snapshot.data!;
-            return Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Colors.brown,
-                Colors.orange,
-              ])),
-              child: Scaffold(
-                appBar: AppBar(
-                  title: Text(cocktail['strDrink']),
-                  elevation: 0.0,
-                ),
-                body: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      future: fetchCocktail(widget.id),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Errornya: ${snapshot.error}'));
+        } else if (!snapshot.hasData) {
+          return Center(child: Text('No data found'));
+        } else {
+          dynamic cocktail = snapshot.data!;
+          return Scaffold(
+            appBar: AppBar(
+              foregroundColor: Colors.white,
+              elevation: 0,
+              backgroundColor: primary,
+              title: Text("Details "),
+            ),
+            backgroundColor: primary,
+            body: SafeArea(
+              child: Stack(
+                children: [
+                  Column(
                     children: [
-                      Text(
-                        cocktail['strDrink'],
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      Image.network(
+                        cocktail['strDrinkThumb'],
+                        fit: BoxFit.fitHeight,
                       ),
-
-                      Hero(
-                        tag: cocktail,
-                        child: CircleAvatar(
-                          radius: 100.0,
-                          backgroundImage: NetworkImage(
-                            cocktail['strDrinkThumb'],
-                          ),
-                        ),
-                      ),
-                      Text(
-                        'Instructions:',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        cocktail['strIngredient1'],
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-
-                      // SizedBox(height: 8),
-                      // Text(
-                      //   'Category: ${cocktail.drinks.first.strCategory}',
-                      //   style: TextStyle(
-                      //     fontSize: 18,
-                      //     color: Colors.grey[600],
-                      //   ),
-                      // ),
-                      // SizedBox(height: 8),
-
-                      // SizedBox(height: 16),
-                      // Image.network(cocktail.drinks.first.strDrinkThumb),
                     ],
                   ),
-                ),
+                  Positioned(
+                    bottom: 0,
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [primary, accent, primary],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        color: primary,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2.5,
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              cocktail['strDrink'],
+                              style: title,
+                            ),
+                            Divider(
+                              color: Colors.white.withOpacity(0.5),
+                              thickness: 1,
+                              height: 20,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  cocktail['strCategory'],
+                                  style: title,
+                                ),
+                                Text(
+                                  cocktail['strAlcoholic'],
+                                  style: title,
+                                ),
+                              ],
+                            ),
+                            Divider(
+                              color: Colors.white.withOpacity(0.5),
+                              thickness: 1,
+                              height: 20,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  cocktail['strInstructions'],
+                                  style: smallcontent,
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      cocktail['strGlass'],
+                                      style: smallcontent,
+                                    ),
+                                    Icon(
+                                      Icons.wine_bar_outlined,
+                                      color: Colors.white.withOpacity(0.5),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Divider(
+                              color: Colors.white.withOpacity(0.5),
+                              thickness: 1,
+                              height: 20,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Ingredient",
+                                  style: title,
+                                ),
+                              ],
+                            ),
+                            buildIngredientRow(cocktail['strIngredient1'],
+                                cocktail['strMeasure1']),
+                            buildIngredientRow(cocktail['strIngredient2'],
+                                cocktail['strMeasure2']),
+                            buildIngredientRow(cocktail['strIngredient3'],
+                                cocktail['strMeasure3']),
+                            buildIngredientRow(cocktail['strIngredient4'],
+                                cocktail['strMeasure4']),
+                            buildIngredientRow(cocktail['strIngredient5'],
+                                cocktail['strMeasure5']),
+                            buildIngredientRow(cocktail['strIngredient6'],
+                                cocktail['strMeasure6']),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            );
-          }
-        });
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget buildIngredientRow(String? ingredient, String? measure) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          ingredient?.toString() ?? '',
+          style: smallcontent,
+        ),
+        Text(
+          measure?.toString() ?? '',
+          style: smallcontent,
+        ),
+      ],
+    );
   }
 }
-    //   child: Container(
-    //     decoration: BoxDecoration(
-    //         gradient: LinearGradient(colors: [
-    //       Colors.brown,
-    //       Colors.orange,
-    //     ])),
-    //     child: Scaffold(
-    //         backgroundColor: Colors.transparent,
-    //         appBar: AppBar(
-    //           backgroundColor: Colors.transparent,
-    //           title: Text("data"),
-    //           elevation: 0.0,
-    //         ),
-    //         body: FutureBuilder(
-    //           future: fetchCocktail(widget.id),
-    //         )),
-    //   ),
-    // );
-
-    //  Center(
-    //   child: Column(
-    //     mainAxisAlignment: MainAxisAlignment.center,
-    //     children: <Widget>[
-       
-    //       SizedBox(
-    //         height: 30.0,
-    //       ),
-    //       Text(
-    //         "ID: ${widget.drink["idDrink"]}",
-    //         style: TextStyle(
-    //           color: Colors.white,
-    //         ),
-    //       ),
-    //       SizedBox(
-    //         height: 30.0,
-    //       ),
-    //       Text(
-    //         "${widget.drink["strDrink"]}",
-    //         style: TextStyle(
-    //           fontSize: 22,
-    //           color: Colors.white,
-    //         ),
-    //       )
-    //     ],
-    //   ),
-    // ),
-
